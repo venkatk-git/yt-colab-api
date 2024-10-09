6fc7b098c969e0c7bdfb617c2f91e0f4b5b24a26
venkatk-git <venkatkumar.profiles@gmail.com>
2024-10-09 20:10:00 +0530

    Setted up socket.io

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const app = require("./app");
+const http = require("http");
+
+const httpServer = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(httpServer);
+
+const uuidv4 = require("uuid").v4;
+
 const { PORT } = require("./config/variables");
 
-app.listen(PORT, () => {
+const connections = {};
+const users = {};
+
+io.on("connection", (socket) => {
+    const uuid = uuidv4();
+    connections[uuid] = socket;
+
+    console.log(connections);
+});
+
+httpServer.listen(PORT, () => {
     console.log(`Server running on PORT : ${PORT}`);
 });
